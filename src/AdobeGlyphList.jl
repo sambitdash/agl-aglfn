29f4ab75ea1cbeabba19cecc5dc52826922e7efa
Sambit Kumar Dash <sambitdash@gmail.com>
2018-03-31 00:15:47 +0530

    0.7 related fixes

--- src/AdobeGlyphList.jl
+++ src/AdobeGlyphList.jl
@@ -6,9 +6,16 @@ export  aglfn,
 
 @static if VERSION >= v"0.7-"
     using DelimitedFiles
+    using Pkg
 end
 
 load_file(filename) = readdlm(filename, ';', String, '\n')
+
+@static if VERSION >= v"0.7-"
+    to_uint16(x) = parse(UInt16, x, base=16)
+else
+    to_uint16(x) = parse(UInt16, x, 16)
+end
 
 """
 ```
@@ -20,8 +27,8 @@ function aglfn()
     path = joinpath(Pkg.dir("AdobeGlyphList"), "aglfn.txt")
     a = load_file(path)
     b = similar(a, Any)
-    b[:,2:3] .= @view a[:,2:3]
-    b[:,1] = map(x->Char(parse(UInt16, x, 16)), @view a[:,1])
+    b[:,2:3] = @view a[:,2:3]
+    b[:,1]   = map(x->Char(to_uint16(x)), @view a[:,1])
     return b
 end
 
@@ -35,10 +42,10 @@ function agl()
     path = joinpath(Pkg.dir("AdobeGlyphList"), "glyphlist.txt")
     a = load_file(path)
     b = similar(a, Any)
-    b[:,1] .= @view a[:,1]
-    b[:,2] .= map(@view a[:,2]) do x
+    b[:,1] = @view a[:,1]
+    b[:,2] = map(@view a[:,2]) do x
         y = split(x, ' ')[1]
-        return Char(parse(UInt16, y, 16))
+        return Char(to_uint16(y))
     end
     return b
 end
@@ -50,11 +57,11 @@ end
     Returns the mapping of glyph code to unicode character code.
 """
 function zapfdingbats()
-    path = joinpath(Pkg.dir("AdobeGlyphList"),"zapfdingbats.txt")
+    path = joinpath(Pkg.dir("AdobeGlyphList"), "zapfdingbats.txt")
     a = load_file(path)
     b = similar(a, Any)
-    b[:,1] .= @view a[:,1]
-    b[:,2] .= map(x->Char(parse(UInt16, x, 16)),@view a[:,2])
+    b[:,1] = @view a[:,1]
+    b[:,2] = map(x->Char(to_uint16(x)), @view a[:,2])
     return b
 end
 
